a4545623a140c648dc94bf142f3e88ecac938f30
Tomo <68368403+tomonorman@users.noreply.github.com>
2026-03-12 16:04:33 +0900

    set interface based on platform (#3)

--- simulate_python/config.py
+++ simulate_python/config.py
@@ -1,7 +1,9 @@
+import platform as _platform
+
 ROBOT = "go2" # Robot name, "go2", "b2", "b2w", "h1", "go2w", "g1" 
 ROBOT_SCENE = "../unitree_robots/" + ROBOT + "/scene.xml" # Robot scene
 DOMAIN_ID = 1 # Domain id
-INTERFACE = "lo" # Interface 
+INTERFACE = "lo0" if _platform.system() == "Darwin" else "lo" # Interface
 
 USE_JOYSTICK = 1 # Simulate Unitree WirelessController using a gamepad
 JOYSTICK_TYPE = "xbox" # support "xbox" and "switch" gamepad layout
